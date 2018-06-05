38aa8c0ff31464f1bcd836a8becfa2ece7d21527
Pavol Halás <pavol.halas1@outlook.com>
2018-06-05 10:52:11 +0200

    added Auxiliary.js, added Layout.js/css, changed location of application

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Navbar from './components/Navbar/Navbar'
+import Layout from './Layout/Layout' 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
-        <div>body, odkazy</div>
-        <div>footer</div>
+        <Layout />
       </div>
     );
   }
---
+++ src/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Layout.css'
+
+import Aux from './../hoc/Auxiliary'
+import Navbar from './../components/Navbar/Navbar'
+const layout = (props) => (
+   <Aux>
+      <Navbar />
+   </Aux>
+)
+
+export default layout
